cf1c49ad7bde30006e7d4f5f2649ee1c94f85e9f
Edgar Núñez <EJNunez1311@users.noreply.github.com>
2019-05-16 21:03:13 -0400

    Update 2
    Edgar Branch!! Need some changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,48 @@ import java.io.IOException;
 
         public class Main {
             public static void main(String[] args) throws IOException {
-                System.out.println("Hola Mundo Java");
-                Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-                log(doc.title());
-                Elements newsHeadlines = doc.select("#mp-itn b a");
-                for (Element headline : newsHeadlines) {
-                    log("%s\n\t%s",
-                            headline.attr("title"), headline.absUrl("href"));
+                Tareas tareas = new Tareas();
+
+                Scanner scanner = new Scanner(System.in);
+
+                char op;
+                boolean bye = true;
+
+                while (true) {
+                    System.out.print("------------------------------------------------------ \n");
+                    System.out.print("Seleccione:\na) Indicar la cantidad de lineas del recurso retornado.\n" +
+                            "b) Indicar la cantidad de parrafos (p) que contiene el documento HTML.\n" +
+                            "c) Indicar la cantidad de imagenes (img) dentro de los parrafos que contiene el archivo HTML.\n" +
+                            "d) Indicar la cantidad de formularios (form) que contiene el HTML por categorizando por el metodo implementado POST o GET.\n" +
+                            "e) Para cada formulario mostrar los campos del tipo input y su respectivo tipo que contiene en el documento HTML.\n" +
+                            "f) Para cada formulario,identificar que el metodo de envio del formulario sea POST y enviar una peticion al servidor con el parametro llamado asignatura y valor practica1 y un header llamado matricula con el valor correspondiente a matricula asignada. Debe mostrar la respuesta por la salida estandar.\n" +
+                            "s) Salir\nSeleccion: ");
+                    op = scanner.next().toUpperCase().charAt(0);
+                    switch (op) {
+                        case 'A':
+                            tareas.parteA();
+                            break;
+                        case 'B':
+                            tareas.parteB();
+                            break;
+                        case 'C':
+                            tareas.parteC();
+                            break;
+                        case 'D':
+                            tareas.parteD();
+                            break;
+                        case 'E':
+                            tareas.parteE();
+                            break;
+                        case 'F':
+                            tareas.parteF();
+                            break;
+                        case 'S':
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.println("Opcion invalida.");
+                    }
                 }
-
-            }
-
-            private static void log(String msg, String... vals) {
-                System.out.println(String.format(msg, vals));
             }
         }
